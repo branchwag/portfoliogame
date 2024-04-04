bf88b1e579a7e4031bd8369c991f6c31c6420f1d
LeAnne B <branchleannelmb@gmail.com>
2024-04-04 17:10:28 -0400

    adds work done so far to github

---
+++ README.md
@@ -0,0 +1,4 @@
+# Game Portfolio
+
+This was built following the lovely tutorial below:</br>
+https://www.youtube.com/watch?v=wy_fSStEgMs
---
+++ src/main.js
@@ -0,0 +1,58 @@
+import { scaleFactor } from "./constants";
+import { k } from "./kaboomCtx";
+
+k.loadSprite("spritesheet", "./spritesheet.png", {
+  sliceX: 39,
+  sliceY: 31,
+  anims: {
+    "idle-down": 952,
+    "walk-down": { from: 952, to: 955, loop: true, speed: 8 }, //try changing this later to be another sprite with dif numbers
+    "idle-side": 991,
+    "walk-side": { from: 991, to: 994, loop: true, speed: 8 },
+    "idle-up": 1030,
+    "walk-up": { from: 1030, to: 1033, loop: true, speed: 8 },
+  },
+});
+
+k.loadSprite("map", "./map.png");
+
+k.setBackground(k.Color.fromHex("#311047"));
+
+k.scene("main", async () => {
+  const mapData = await (await fetch("./map.json")).json();
+  const layers = mapData.layers;
+
+  const map = k.make([k.sprite("map"), k.pos(0), k.scale(scaleFactor)]);
+
+  const player = k.make([
+    k.sprite("spritesheet", { anim: "idle-down" }),
+    k.area({ shape: new k.Rect(k.vec2(0, 3), 10, 10) }),
+    k.body(),
+    k.anchor("center"),
+    k.pos(),
+    k.scale(scaleFactor),
+    {
+      speed: 250,
+      direction: "down",
+      isInDialogue: false,
+    },
+    "player",
+  ]);
+
+  for (const layer of layers) {
+    if (layer.name === "boundaries") {
+      for (const boundary of layer.Objects) {
+        map.add([
+          k.area({
+            shape: new k.Rect(k.vec2(0), boundary.width, boundary.height),
+          }),
+          k.body({ isStatic: true }),
+          k.pos(boundary.x, boundary.y),
+          boundary.name,
+        ]);
+      }
+    }
+  }
+});
+
+k.go("main");
