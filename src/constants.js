2fe547078967db0fca892e135ab53c4a6da010aa
LeAnne B <branchleannelmb@gmail.com>
2024-04-11 22:01:30 -0400

    fixes small typo

--- src/constants.js
+++ src/constants.js
@@ -5,7 +5,7 @@ export const dialogueData = {
     Here is my <a href="https://github.com/branchwag" target="_blank">Github</a>!`,
   "cs-degree": `This is my degree. I majored in Finance, which involves a lot of math!`,
   "sofa-table": `That's my sofa. I like to chill and watch some awesome programming content creators on YouTube. Go sub to <a href="https://youtube.com/@jslegenddev" target="_blank">this person's channel</a>!
-  You'll learn how to built this portfolio that you're currently playing through!`,
+  You'll learn how to build this portfolio that you're currently playing through!`,
   tv: `That's my TV. Funnily enough, I actually don't have one in real life!`,
   bed: `This where I sleep. I live in NYC so I get to look out at some super tall buildings from my window here.`,
   resume: `This is my desk and on it is my resume. Contact me at branchleannelmb@gmail.com if you would like to check it out!`,
