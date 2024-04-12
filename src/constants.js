d88998d70f8bc1b6bea80acac0f4b46f458a501b
LeAnne B <branchleannelmb@gmail.com>
2024-04-11 21:51:43 -0400

    adds in dialogue

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,17 @@
 export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I work on stuff for trading engines for my main job and do games and other things on the side.  
+    Here is my <a href="https://github.com/branchwag" target="_blank">Github</a>!`,
+  "cs-degree": `This is my degree. I majored in Finance, which involves a lot of math!`,
+  "sofa-table": `That's my sofa. I like to chill and watch some awesome programming content creators on YouTube. Go sub to <a href="https://youtube.com/@jslegenddev" target="_blank">this person's channel</a>!
+  You'll learn how to built this portfolio that you're currently playing through!`,
+  tv: `That's my TV. Funnily enough, I actually don't have one in real life!`,
+  bed: `This where I sleep. I live in NYC so I get to look out at some super tall buildings from my window here.`,
+  resume: `This is my desk and on it is my resume. Contact me at branchleannelmb@gmail.com if you would like to check it out!`,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
+  Kaboom.js by watching some of the amazing tutorials <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
+  library: `I have a lot of random books here. I really loved the Harry Potter series and have a giant book of Edgar Allen Poe stories here.`,
+  exit: `If you want to exit this portfolio, just close the tab!`,
+};
